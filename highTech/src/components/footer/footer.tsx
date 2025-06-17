2ac827d157c79e125a6ab3c2834a34e6f45bff12
David Krstevski <krstevskidavid998@gmail.com>
2025-06-17 19:32:28 +0200

    add button on home page

--- highTech/src/components/footer/footer.tsx
+++ highTech/src/components/footer/footer.tsx
@@ -2,25 +2,31 @@ import "./Footer.css";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        <div>
-          <h3 className="footer-title">HighTech Service</h3>
-          <p className="footer-text">© 2025 All rights reserved.</p>
-        </div>
-        <div>
-          <h4 className="footer-subtitle">Contact Us</h4>
-          <p className="footer-text">hightechservice.mk@gmail.com</p>
-          <p className="footer-text">+389 76 910 836</p>
-        </div>
-        <div>
-          <h4 className="footer-subtitle">Follow Us</h4>
-          <a href="#" className="footer-link">Facebook</a> 
-          <hr />
-          <a href="#" className="footer-link">Instagram</a> 
-        </div>
-      </div>
-    </footer>
+ <footer className="footer">
+  <div className="footer-container">
+    <div>
+      <h3 className="footer-title">HighTech Service</h3>
+      <p className="footer-text">© 2025 All rights reserved.</p>
+    </div>
+    <div>
+      <h4 className="footer-subtitle">Contact Us</h4>
+      <p className="footer-text">hightechservice.mk@gmail.com</p>
+      <p className="footer-text">+389 76 910 836</p>
+    </div>
+    <div>
+      <h4 className="footer-subtitle">Follow Us</h4>
+     <a
+  href="https://www.facebook.com/profile.php?id=61573859452758"
+  className="footer-link"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <i className="fab fa-facebook-f facebook-icon"></i>
+  Facebook
+</a>
+    </div>
+  </div>
+</footer>
   );
 };
 
--- highTech/src/components/homePage/homePage.tsx
+++ highTech/src/components/homePage/homePage.tsx
@@ -1,21 +1,24 @@
 import "./HomePage.css";
 import hero from "../../assets/herroImg.png";
+import { useNavigate } from "react-router-dom";
 
 
 export const HomePage = () => {
- 
+  const navigate = useNavigate();
   return (
     <>
-      <section className="hero">
-        <div className="hero-content">
-          <h1>Брзо. Сигурно. Ефикасно.</h1>
-          <p>
-            Сервис за компјутери и лаптопи со кој можете да сметате. Поправка, надградба и оптимизација без компромис.</p>
-          <div className="hero-buttons">
-          </div>
+   <section className="hero">
+      <div className="hero-content">
+        <h1>Брзо. Сигурно. Ефикасно.</h1>
+        <p>
+          Сервис за компјутери и лаптопи со кој можете да сметате. Поправка, надградба и оптимизација без компромис.
+        </p>
+        <div className="button">
+          <button onClick={() => navigate("/Services")}>УСЛУГИ</button>
         </div>
-        <img src={hero} alt="Hero Image" className="hero-img" />
-      </section>
+      </div>
+      <img src={hero} alt="Hero Image" className="hero-img" />
+    </section>
     </>
   );
 };
